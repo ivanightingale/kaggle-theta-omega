d926de0f7b29f60f8a7685221ef9a1ef97303ae8
ivanightingale <guancheq@usc.edu>
2019-03-29 23:06:08 -0700

    SVM

--- titanic/cleanTitanic.m
+++ titanic/cleanTitanic.m
@@ -1,31 +1,28 @@
 function tbl = cleanTitanic(tbl)
 
-    tbl.Fare(tbl.Fare == 0) = NaN;      % treat 0 fare as NaN
+    tbl.Fare(tbl.Fare == 0) = NaN;  % treat 0 fare as NaN
 
     tbl.Sex = double(tbl.Sex);
-
-    tbl.Age(isnan(tbl.Age)) = nanmean(tbl.Age);  % replace NaN with the average
+    
+    tbl.Age(isnan(tbl.Age)) = nanmean(tbl.Age);  % replace NaN with mean
     % categorize age groups
-    tbl.AgeGroup = double(discretize(tbl.Age, [0:10:20, 65, 80], 'categorical',{'child','teen','adult','senior'}));
+    tbl.AgeGroup = discretize(tbl.Age, [0:10:20, 65, 80], 'categorical', {'child','teen','adult','senior'});
 
-    tbl.Embarked(isundefined(tbl.Embarked)) = mode(tbl.Embarked);
+    tbl.Embarked(isundefined(tbl.Embarked)) = mode(tbl.Embarked);  % replace undefined with mode
     tbl.Embarked = double(tbl.Embarked);
 
-    fare = grpstats(tbl(:,{'Pclass','Fare','Embarked'}),{'Pclass','Embarked'});
+    fare = grpstats(tbl(:,{'Pclass','Fare','Embarked'}),{'Pclass','Embarked'}, 'nanmean');
     for i = 1 : height(fare)
-        % apply the average fare based on class and embarked to missing values
-        tbl.Fare(tbl.Pclass == fare.Pclass(i) & tbl.Embarked == fare.Embarked(i) & isnan(tbl.Fare)) = fare.mean_Fare(i);
+        % replace NaN with average fare based on class and embarked to missing values
+        tbl.Fare(isnan(tbl.Fare) & tbl.Pclass == fare.Pclass(i) & tbl.Embarked == fare.Embarked(i)) = fare.nanmean_Fare(i);
     end
-    tbl.FareRange = double(discretize(tbl.Fare, [0:10:30, 100, 520], 'categorical',{'<10','10-20','20-30','30-100','>100'}));
-
-    % split strings by white space
-    tbl_cabins = cellfun(@strsplit, tbl.Cabin, 'UniformOutput', false);
-    % count the number of tokens
-    tbl.nCabins = cellfun(@length, tbl_cabins);
-    % deal with exceptions - only the first class people had multiple cabins
-    tbl.nCabins(tbl.Pclass ~= 1 & tbl.nCabins > 1,:) = 1;
-    % if Cabin is empty, then nCabins should be 0
-    tbl.nCabins(cellfun(@isempty, tbl.Cabin)) = 0;
+    tbl.FareRange = discretize(tbl.Fare, [0:10:30, 100, 520], 'categorical', {'<10','10-20','20-30','30-100','>100'});
+    
+    tbl.Pclass = categorical(tbl.Pclass, [3, 2, 1], 'Ordinal', true);
+    tbl.Sex = categorical(tbl.Sex);
+    tbl.Embarked = categorical(tbl.Embarked);
+    tbl.AgeGroup = categorical(tbl.AgeGroup,{'child','teen','adult','senior'}, 'Ordinal', true);
+    tbl.FareRange = categorical(tbl.FareRange, {'<10','10-20','20-30','30-100','>100'}, 'Ordinal', true);
     
     tbl(:,{'Name','Ticket','Cabin'}) = [];
 end
--- titanic/titanic.m
+++ titanic/titanic.m
@@ -1,14 +1,16 @@
+clear all;
+
 train = readtable('train.csv','Format','%f%f%f%q%C%f%f%f%q%f%q%C');
 test = readtable('test.csv','Format','%f%f%q%C%f%f%f%q%f%q%C');
 
 train = cleanTitanic(train);
+train.Survived = categorical(train.Survived);
 test = cleanTitanic(test);
 
-[bf, bfScore] = trainTitanicClassifier(train);
-disp(bfScore)
-% one of the benefits of Random Forest is its feature importance metric
+[gaussian, gaussianScore] = trainTitanicSVM(train);
+disp(gaussianScore)
 
-PassengerId = test.PassengerId;             % extract Passenger Ids
-Survived = bf.predictFcn(test);             % generate response variable
-submission = table(PassengerId, Survived);   % combine them into a table
-writetable(submission,'submission.csv')     % write to a CSV file
+Survived = gaussian.predictFcn(test);
+PassengerId = test.PassengerId;
+submission = table(PassengerId, Survived);
+writetable(submission,'submission.csv');
